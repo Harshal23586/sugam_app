f0d083075038cb76980c71a66142e192b93706c9
harshalkotwal <47349607+Harshal23586@users.noreply.github.com>
2025-12-03 12:09:22 +0530

    Update helpers.py

---
+++ utils/helpers.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+import pandas as pd
+
+def get_document_requirements_by_parameters(approval_type):
+    """Get document requirements organized by parameters"""
+    # ... (move this function here)
+    pass
+
+def get_institution_performance(institution_id, analyzer):
+    """Get institution performance data"""
+    # ... (move this function here)
+    pass
+
+# ... (move other helper functions here)
