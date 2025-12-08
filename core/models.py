aac802d1b6972788afdd48560afc8eadc84a0331
harshalkotwal <47349607+Harshal23586@users.noreply.github.com>
2025-12-08 21:55:05 +0530

    Create models.py

---
+++ core/models.py
@@ -0,0 +1,9 @@
+# Placeholder Models for AI / Scoring / Classification
+# Extend as per your use-case
+
+class BaseModel:
+    def __init__(self):
+        pass
+
+    def predict(self, x):
+        return "Not Implemented"
